c73b21c85bb4c684dc9e4964a70fc72c74b2b33b
Aychtee <aychteeht@gmail.com>
2021-07-20 23:26:33 -0700

    edit

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -5,10 +5,16 @@ const EventComponent: React.FC = () => {
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event)
     }
+    //apply a drag event type to the event
+    const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+        console.log(event)
+    }
 
     return <div>
         {/* with an inline function e=>console.log(e) ts understands/inferences that onChange is a prop and that we have to provide some sort of callback function to it. it will have a first argument,in this case 'event', and it's type will be a react.ChangeEvent<htmlinputelement>. The type inference is not applied if you define your own function, similar to above. you will have to manually enter the type for the argument.*/}
         <input onChange={onChange} />
+        {/* create a drag event */}
+        <div draggable onDragStart={onDragStart}>Drag Me!</div>
     </div>
 }
 export default EventComponent
